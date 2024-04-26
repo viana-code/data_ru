74fd1cb59b67ac1f171ce440f9fc9df5ecb6d095
guiaraujoreal <joaoguilhermearaujo1617@gmail.com>
2024-04-26 16:29:04 -0300

    Alteracoes

--- data-ru/src/components/Main/Main.jsx
+++ data-ru/src/components/Main/Main.jsx
@@ -1,9 +1,11 @@
 import { Inicio } from "../Inicio/Inicio"
+import  StarRating  from '../StarRating/StarRating'
 
 export const Main = () => {
   return (
     <main className="d-flex justify-content-center" style={{width: '100%', marginTop: '50px'}}>
         <Inicio/>
+        <StarRating/>
     </main>
   )
 }
